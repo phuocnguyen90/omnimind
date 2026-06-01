165de2f93669569ba860714f5397489f4ca93888
phuocnguyen90 <phuocnguyen90@yahoo.com>
2026-06-01 09:56:31 +0700

    chore: cleanup stale files and fix gitignore encoding

--- test_tracker.ts
+++
@@ -1,8 +0,0 @@
-import { SyncTracker } from './src/ingestion/tracker';
-import * as path from 'path';
-import * as os from 'os';
-
-const workspaceDir = path.join(os.homedir(), ".omnimind");
-const tracker = new SyncTracker(workspaceDir);
-tracker.markZoteroComplete("TEST_KEY");
-console.log("hasZotero:", tracker.hasZotero("TEST_KEY"));
